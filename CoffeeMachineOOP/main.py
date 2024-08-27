bf67a82e57ca8c22f9f59d46c29a6233de0b7d3a
amiinssh <145289316+amiinssh@users.noreply.github.com>
2024-08-27 04:21:12 -1200

    Add files via upload

---
+++ CoffeeMachineOOP/main.py
@@ -0,0 +1,31 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+def main():
+    coffee_maker = CoffeeMaker()
+    menu = Menu()
+    money_machine = MoneyMachine()
+
+    is_on = True
+
+    while is_on:
+        options = menu.get_items()
+        choice = input(f"What would you like? ({options}): ")
+
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        else:
+            drink = menu.find_drink(choice)
+            cost = drink.cost
+
+            if drink and coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(cost):
+                coffee_maker.make_coffee(drink)
+
+
+if __name__ == "__main__":
+    main()
